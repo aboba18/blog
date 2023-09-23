d6aa60f3e677a7c726bfd0024fb4c375d29a270b
aboba18 <12345nesterenko@gmail.com>
2023-09-23 12:36:15 +0300

    create db stuture

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    canegory_name = models.TextField(max_length=25)
+
+
+
+class Post(models.Model):
+    post_header = models.CharField(max_length=50)
+    post_text = models.TextField(max_length=120)
+    author = models.CharField(max_length=12)
+    datetime = models.DateTimeField()
+    category =  models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default='no category')
+
