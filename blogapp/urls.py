9bdb01f240cdfc6329a21b6814b7c0abad8e55f6
aboba18 <104203631+aboba18@users.noreply.github.com>
2023-09-30 21:40:02 +0300

    add comment

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', view=views.index, name="home"),
+    #робить посилання для постів
     path("post/<int:pk>", view=views.PostDetailView.as_view(), name="post-details")
 ]
