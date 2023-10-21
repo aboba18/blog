025f7f336aa786dd90389820658086d926cddb50
aboba18 <12345nesterenko@gmail.com>
2023-10-21 13:27:37 +0300

    add images

--- blog/settings.py
+++ blog/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -121,6 +122,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS =[
     BASE_DIR / 'static',
 ]
+
+MEDIA_URL="/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
