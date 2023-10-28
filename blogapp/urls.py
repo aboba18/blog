c9d13df4f4d9661cb784a25e940ae18510254830
aboba18 <12345nesterenko@gmail.com>
2023-10-28 13:33:04 +0300

    add url for author and category, fixed css

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -8,5 +8,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', view=views.index, name="home"),
     #робить посилання для постів
-    path("<int:pk>", view=views.PostDetailView.as_view(), name="post-details")
+    path("<int:pk>", view=views.PostDetailView.as_view(), name="post-details"),
+    path("category", view=views.CategoryList.as_view(), name = "category_list"),
+    path("author", view=views.AuthorList.as_view(), name = "author_list"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from .models import Post
-from django.views.generic import DetailView
+from .models import Post, Category
+from django.views.generic import DetailView, ListView
+
 # Create your views here.
 def index(req):
     post_list = Post.objects.all()
+
+    if req.GET.get("author", False):
+        author = req.GET.get("author", False)
+        post_list = Post.objects.filter(author__exact=author) 
+
+
+    if req.GET.get("category", False):
+        category = req.GET.get("category", False)
+        post_list = Post.objects.filter(category__canegory_name=category)
 
 
     return render(req, "blogapp/index.html", context={"posts": post_list})
@@ -12,3 +22,32 @@ class PostDetailView(DetailView):
     template_name = "blogapp/post_detail.html"
 
     model = Post
+
+
+
+class CategoryList(ListView):
+    template_name ="blogapp/category_list.html"
+
+    model = Category
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+
+        context["category_list"] = self.get_queryset()
+
+        return context
+
+
+class AuthorList(ListView):
+    template_name ="blogapp/author_list.html"
+
+    model = Post
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["author_list"] = []
+        for post in self.get_queryset():
+            if not post.author in context['author_list']:
+                context["author_list"].append(post.author)
+
+        return context
